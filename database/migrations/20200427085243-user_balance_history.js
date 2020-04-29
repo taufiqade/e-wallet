29a88864fc17ad011fedb23305da621096087848
taufiqade <taufiqadesurya@gmail.com>
2020-04-29 20:23:30 +0700

    minor changes seeders

--- database/migrations/20200427085243-user_balance_history.js
+++ database/migrations/20200427085243-user_balance_history.js
@@ -46,6 +46,15 @@ module.exports = {
       author: {
         type: Sequelize.STRING,
         allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
       }
     }).then(()=>{
       queryInterface.addConstraint('user_balance_history', ['user_balance_id'],{
--- database/seeders/20200428121343-users.js
+++ database/seeders/20200428121343-users.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('users', [{
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('users', [{
       name: 'user1',
       email: 'user1@test.com',
       password:'test123', 
@@ -14,10 +14,29 @@ module.exports = {
       password:'test123', 
       created_at: new Date(),
       updated_at: new Date()
-    }]);
+    }])
+    
+    const user = await queryInterface.sequelize.query(
+      `SELECT id from users;`
+    );
+    const userRow = user[0];
+    await queryInterface.bulkInsert('user_balance', [{
+      user_id: userRow[0].id,
+      balance: 1000000,
+      balance_achieve: 1000000,
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      user_id: userRow[1].id,
+      balance: 1000000,
+      balance_achieve: 1000000,
+      created_at: new Date(),
+      updated_at: new Date()
+    }])
   },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('users', null, {});
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('users', null, {});
+    await queryInterface.bulkDelete('users', null, {});
   }
 };
